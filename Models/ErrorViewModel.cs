9c81d3d6ee8b952ab18b0a600015ca68d7f058c3
Daniel <danielolicristian12@gmail.com>
2022-11-23 21:30:20 -0300

    atividade_TEC_PROGRAMAÇÂO

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TEC_progama_o.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
